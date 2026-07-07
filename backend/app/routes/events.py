6990bc843efacd5930ba5b50c7e5c2dfb115e588
MaAs0706e <aswanthmadhav07@gmail.com>
2026-07-07 11:11:14 +0530

    created the get events by id functionality

--- backend/app/routes/events.py
+++ backend/app/routes/events.py
@@ -34,3 +34,14 @@ def create_event(
 def get_events(db: Session = Depends(get_db)):
 
     return db.query(Event).all()
+
+@router.get("/events/{event_id}")    
+def get_event(
+    event_id:int ,
+    db:session = Depends(get_db)
+
+):
+    event = db.query(Event).filter(Event.id == event_id).first()
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return event
