b264c59cae1f184b1e3fc865c644e08468a4113b
MaAs0706e <aswanthmadhav07@gmail.com>
2026-07-07 11:17:51 +0530

    writing the event update functionality but there are some errors like the whole details have to be udated so will have to fix that

--- backend/app/routes/events.py
+++ backend/app/routes/events.py
@@ -3,6 +3,8 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.models.event import Event 
 
+from app.schemas.event import EventUpdate
+
 from app.dependencies import get_db
 from app.schemas.event import EventCreate
 router = APIRouter()
@@ -44,4 +46,72 @@ def get_event(
     event = db.query(Event).filter(Event.id == event_id).first()
     if not event:
         raise HTTPException(status_code=404, detail="Event not found")
+    return event
+
+@router.put("/events/{event_id}")
+def update_event(
+    event_id: int,
+    updated_event: EventCreate,
+    db: Session = Depends(get_db)
+):
+
+    event = (
+        db.query(Event)
+        .filter(Event.id == event_id)
+        .first()
+    )
+
+    if not event:
+        raise HTTPException(
+            status_code=404,
+            detail="Event not found"
+        )
+
+    event.title = updated_event.title
+    event.description = updated_event.description
+    event.category = updated_event.category
+    event.venue = updated_event.venue
+    event.date = updated_event.date
+    event.status = updated_event.status
+    event.organizer = updated_event.organizer
+    event.attendees = updated_event.attendees
+    event.capacity = updated_event.capacity
+    event.image = updated_event.image
+
+    db.commit()
+    db.refresh(event)
+
+    return event
+
+
+@router.patch("/events/{event_id}")
+def update_event(
+    event_id: int,
+    updates: EventUpdate,
+    db: Session = Depends(get_db)
+):
+
+    event = (
+        db.query(Event)
+        .filter(Event.id == event_id)
+        .first()
+    )
+
+    if not event:
+        raise HTTPException(
+            status_code=404,
+            detail="Event not found"
+        )
+
+    update_data = updates.model_dump(
+        exclude_unset=True
+    )
+
+    for key, value in update_data.items():
+        setattr(event, key, value)
+
+    db.commit()
+
+    db.refresh(event)
+
     return event
--- backend/app/schemas/event.py
+++ backend/app/schemas/event.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from typing import List, Optional
+
 
 class EventCreate(BaseModel):
 
@@ -22,3 +24,16 @@ class EventCreate(BaseModel):
     capacity: int
 
     image: str
+
+class EventUpdate(BaseModel):
+
+    title: Optional[str] = None
+    description: Optional[str] = None
+    category: Optional[str] = None
+    venue: Optional[str] = None
+    date: Optional[str] = None
+    status: Optional[str] = None
+    organizer: Optional[str] = None
+    attendees: Optional[int] = None
+    capacity: Optional[int] = None
+    image: Optional[str] = None    
