34ca92f724a6413f3d69729b668c151cb2f742a1
MaAs0706e <aswanthmadhav07@gmail.com>
2026-07-15 14:35:44 +0530

    updating the jwt.py to get the access token

---
+++ backend/app/utils/jwt.py
@@ -0,0 +1,48 @@
+from jose import jwt
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
+expire = (
+    datetime.now(timezone.utc)
+    + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+)
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+)
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+    expire = (
+    datetime.now(timezone.utc)
+    + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+)
+
+    to_encode.update(
+        {
+            "exp": expire
+        }
+    )
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+    
