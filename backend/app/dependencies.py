5e509b16fbe01c21b5c3fd53b6b7a4b2fe200e65
MaAs0706e <aswanthmadhav07@gmail.com>
2026-07-16 20:09:53 +0530

    updating hte dependencies.py

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,10 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
 from app.database import SessionLocal
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi.security import HTTPBearer
+from jose import jwt
+from jose import JWTError
+from app.models.user import User
 
 
 security = HTTPBearer()
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 def get_db():
 
@@ -20,7 +31,7 @@ def get_current_user(
 
     token = Depends(security),
 
-    db: Session = Depends(get_db)
+    db:Session = Depends(get_db)
 
 ):
 
@@ -36,14 +47,18 @@ def get_current_user(
             SECRET_KEY,
             algorithms=[ALGORITHM]
         )
+        print(token.credentials)
+
+        print("PAYLOAD:", payload)
 
         user_id = payload.get("user_id")
 
         if user_id is None:
             raise credentials_exception
 
-    except JWTError:
+    except JWTError as e :
 
+        print("JWT Error:", e)
         raise credentials_exception
 
     user = (
