0188b475fd398230ce923bf1ad37fafbc3cf2bba
MaAs0706e <aswanthmadhav07@gmail.com>
2026-07-30 11:19:50 +0530

    Add campus watermark to application pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,18 +11,26 @@ import UserProfile from './pages/UserProfile'
 
 import './index.css'
 
+function WatermarkPage({ children }) {
+  return (
+    <div className="campus-watermark">
+      {children}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard/student" element={<StudentDashboard />} />
-        <Route path="/dashboard/coordinator" element={<CoordinatorDashboard />} />
-        <Route path="/dashboard/approver" element={<ApproverDashboard />} />
-        <Route path="/dashboard/admin" element={<AdminDashboard />} />
-        <Route path="/events/:id" element={<EventDetails />} />
-        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/login" element={<WatermarkPage><Login /></WatermarkPage>} />
+        <Route path="/dashboard/student" element={<WatermarkPage><StudentDashboard /></WatermarkPage>} />
+        <Route path="/dashboard/coordinator" element={<WatermarkPage><CoordinatorDashboard /></WatermarkPage>} />
+        <Route path="/dashboard/approver" element={<WatermarkPage><ApproverDashboard /></WatermarkPage>} />
+        <Route path="/dashboard/admin" element={<WatermarkPage><AdminDashboard /></WatermarkPage>} />
+        <Route path="/events/:id" element={<WatermarkPage><EventDetails /></WatermarkPage>} />
+        <Route path="/profile" element={<WatermarkPage><UserProfile /></WatermarkPage>} />
       </Routes>
     </Router>
   )
